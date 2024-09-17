077d51aafd9ff3e5b5db22f4e3891c3975871802
PhamQuang123 <phamtienquang57@gmail.com>
2024-09-17 10:08:05 +0700

    first commit

---
+++ src/main/java/example/qltccn/QltccnApplication.java
@@ -0,0 +1,13 @@
+package example.qltccn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QltccnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QltccnApplication.class, args);
+    }
+
+}
